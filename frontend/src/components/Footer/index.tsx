d40ec41e2139f6432761563dd2b25355f6b208a8
Matheus Manzutti <mateusmanzutti@gmail.com>
2021-09-06 20:10:33 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/matheus-de-louren%C3%A7o-manzutti-b8063121a/" target="_blank" rel="noreferrer">Matheus Manzutti</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
